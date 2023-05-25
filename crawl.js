584d175b2d636cec08f9e14bf8abf21e7e73c4e2
Ayush <ayushdhokriya823@gmail.com>
2023-05-26 00:34:54 +0530

    geturlsfromhtml

--- crawl.js
+++ crawl.js
@@ -1,3 +1,25 @@
+const { JSDOM } = require("jsdom");
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) == "/") {
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(err.message);
+      }
+    } else {
+      const urlObj = new URL(`${baseURL}`);
+      urls.push(urlObj.href);
+    }
+  }
+  return urls;
+}
+
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
   const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -9,4 +31,5 @@ function normalizeURL(urlString) {
 
 module.exports = {
   normalizeURL,
+  getURLsFromHTML,
 };
